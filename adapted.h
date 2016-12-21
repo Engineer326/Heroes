718e69009ff6f1c61a85296bba04d2f49f6f47cb
Engineer326 <a.demidov13@gmail.com>
2016-12-21 04:14:46 +0200

    Класс адаптер, связанный с Wizard

---
+++ adapted.h
@@ -0,0 +1,26 @@
+#ifndef ADAPTED
+#define ADAPTED
+
+#include "create_hero.h"
+#include "group.h"
+
+class FromMagicPower {
+  public:
+    virtual int toAttack() = 0;
+    virtual ~FromMagicPower() {}
+};
+
+class Adapter: public FromMagicPower {
+public:
+    Adapter( WizardS* w) : wizard_(w) {}
+    ~Adapter() {delete wizard_; }
+    int toAttack() {
+        return wizard_ -> getStrength() + 100;
+    }
+private:
+    WizardS* wizard_;
+};
+
+
+#endif // ADAPTED
+
